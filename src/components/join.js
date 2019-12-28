97d52ac467f0eb1be5433751b22643f606b53da4
Mupati <scientificgh@gmail.com>
2019-12-28 12:45:20 +0000

    End of lesson 10

---
+++ src/components/join.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react"
+import useTwilioVideo from "../hooks/use-twilio-video"
+import { navigate } from "gatsby"
+
+const Join = ({ location }) => {
+  const defaultRoom =
+    (location && location.state && location.state.roomName) || ""
+  const { state, getRoomToken } = useTwilioVideo()
+  const [identity, setIdentity] = useState("")
+  const [roomName, setRoomName] = useState(defaultRoom)
+
+  useEffect(() => {
+    if (state.token && state.roomName) {
+      navigate(`/room/${state.roomName}`)
+    }
+  }, [state])
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    getRoomToken({ identity, roomName })
+  }
+
+  return (
+    <>
+      <h1>Start or Join a Video Call</h1>
+      <form className="start-form" onSubmit={handleSubmit}>
+        <label htmlFor="identity">
+          Display name:
+          <input
+            type="text"
+            id="identity"
+            value={identity}
+            onChange={event => setIdentity(event.target.value)}
+          />
+        </label>
+
+        <label htmlFor="roomName">
+          Which room do you want to join?
+          <input
+            type="text"
+            id="roomName"
+            value={roomName}
+            onChange={event => setRoomName(event.target.value)}
+          />
+        </label>
+
+        <button type="submit">Join Video Chat</button>
+      </form>
+    </>
+  )
+}
+
+export default Join
